102152c3e3b5fbd63a9abf3f5ddcc6f3aaadc2af
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-11-17 14:35:29 +0900

    Add function to read and plot all csv data

--- read_csv.py
+++ read_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import tight_layout
 
 
 # Read and plot csv data 1 by 1
@@ -29,4 +30,58 @@ def one_file(path):
 
     plt.savefig('tV_plot/'+file_name + '.png', dpi=300)
     plt.show()
-one_file('TX5-RX1.csv')
+#one_file('TX5-RX1.csv')
+
+
+# Rea and plot all csv data 
+def all_file():
+    
+    data_51 = pd.read_csv('TX5-RX1.csv', header=None, skiprows=19)
+    data_52 = pd.read_csv('TX5-RX2.csv', header=None, skiprows=19)
+    data_53 = pd.read_csv('TX5-RX3.csv', header=None, skiprows=19)
+    data_54 = pd.read_csv('TX5-RX4.csv', header=None, skiprows=19)
+    data_Th = pd.read_csv('Through.csv', header=None, skiprows=19)
+
+    Time = data_51[0] # Time [s]
+    Input_51 = data_51[1] # Voltage applied to VCO [V]
+    Output_51 = data_51[2] # IF output [V]
+    Output_52 = data_52[2] # IF output [V]
+    Output_53 = data_53[2] # IF output [V]
+    Output_54 = data_54[2] # IF output [V]
+    Output_th = data_Th[2] # IF output [V]
+
+
+    # Plot the data
+    fig, ax = plt.subplots(6, 1, sharex='all', tight_layout=True, figsize=(8, 10))
+
+    ax[0].plot(Time, Input_51)
+    ax[0].set_title('Input', size = 16)
+
+    ax[1].plot(Time, Output_51)
+    ax[1].set_title('Output TX5-RX1', fontsize = 16)
+    ax[1].set_ylim(0.17, 0.31)
+
+    ax[2].plot(Time, Output_52)
+    ax[2].set_title('Output TX5-RX2', fontsize = 16)
+    ax[2].set_ylim(0.17, 0.31)
+
+    ax[3].plot(Time, Output_53)
+    ax[3].set_title('Output TX5-RX3', fontsize = 16)
+    ax[3].set_ylim(0.17, 0.31)
+
+    ax[4].plot(Time, Output_54)
+    ax[4].set_title('Output TX5-RX4', fontsize = 16)
+    ax[4].set_ylim(0.17, 0.31)
+
+    ax[5].plot(Time, Output_th)
+    ax[5].set_title('Output Through', fontsize = 16)
+    ax[5].set_ylim(0.17, 0.31)
+
+
+    fig.supxlabel('Time [s]', size = 14)
+    fig.supylabel('Voltage [V]', size = 14)
+
+
+    plt.savefig('tV_plot/all.png', dpi=300)
+    plt.show()
+all_file()
